f1f50597ead47306e2419de563f58260a0678c29
theeurbanlegend <mbuto152@gmail.com>
2023-06-15 21:57:03 +0300

    Debug

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const accordion=document.getElementsByClassName("main-container")
+const accordion=document.getElementsByClassName("container")
 
 for(let i=0;i<accordion.length;i++){
-    accordion[i].addEventListener('click', function(){
-       this.classList.toggle("active")
+    accordion[i].addEventListener('click', function(e){
+let target=e.target
+/* if(target==qn){
+       this.classList.toggle("active") */
+       if(target){
+        this.classList.toggle("active")
+console.log(target)
+       }
     })
 }
